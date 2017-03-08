c7bd120028f21d2cdedf08b0f0cf170d9629a467
Jeremy Rosser <jrosser@gmail.com>
2017-03-08 16:16:18 -0500

    Boolean added

--- Swift App/ViewController.swift
+++ Swift App/ViewController.swift
@@ -15,37 +15,41 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var text2: UITextField!
     
-
+    
     @IBOutlet weak var theLabel: UILabel!
     
     
     
     @IBAction func buttonTapped(_ sender: Any) {
         
+        let addition = true
         
-        //Adding two text boxes numeric values
-        theLabel.text = "Answer is... \(Double(text1.text!)! + Double(text2.text!)!)"
-       
+        if addition {
+            
+            //Adding two text boxes numeric values
+            theLabel.text = "Answer is... \(Double(text1.text!)! + Double(text2.text!)!)"
+            
+        } else {
+            theLabel.text = "Answer is... \(Double(text1.text!)! + Double(text2.text!)!)"
+        }}
         
-    }
-    
-    @IBAction func button2Tapped(_ sender: Any) {
+        @IBAction func button2Tapped(_ sender: Any) {
+            
+            self.theLabel.text = "Buttons are cool!"
+        }
+        override func viewDidLoad() {
+            super.viewDidLoad()
+            // Do any additional setup after loading the view, typically from a nib.
+            
+            
+            
+        }
         
-        self.theLabel.text = "Buttons are cool!"
-    }
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        override func didReceiveMemoryWarning() {
+            super.didReceiveMemoryWarning()
+            // Dispose of any resources that can be recreated.
+        }
         
-       
         
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
 
